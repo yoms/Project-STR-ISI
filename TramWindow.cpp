3e19201d975c4ce1254aa96d1c60c8242894b3af
Yoms <hormiere.guillaume@gmail.com>
2011-03-12 11:36:52 +0100

    Modification du trajet

--- TramWindow.cpp
+++ TramWindow.cpp
@@ -20,6 +20,10 @@ TramWindow::TramWindow(QWidget *parent) :
     m_tramList << tram;
     tram->setTrajet(m_trajetList[0]);
     tram->start();
+    Tram * tram2 = new Tram;
+    m_tramList << tram2;
+    tram2->setTrajet(m_trajetList[1]);
+    tram2->start();
     setupDrawingWidget();
 
 }
@@ -49,70 +53,95 @@ void TramWindow::setupDrawingWidget()
 void TramWindow::setupTrajet()
 {
     QList<QPoint> tra;
-    QPoint last;
+    QPoint last(25,25);
     double radius = 0.05;
-    Station* s1 = new Station(Station::Terminus);
-    s1->setCoordonnee(QPoint(25,30));
-    s1->setNom("Depart");
-    m_obstacleList << s1;
 
-    for(int i = 25; i < 155; i++)
-            tra << QPoint(i,25);
+
+    for(double i = 0; i < 50; i++)
+        tra << QPoint(last.x()+i, last.y());
 
     last = tra.last();
-    Station* s3 = new Station(Station::NonTerminus);
-    s3->setCoordonnee(QPoint(120,30));
-    s3->setLieu(QPoint(120,25));
-    s3->setNom("Fonbeauzard");
-    m_obstacleList << s3;
-
-    last = tra.last();
-    for(double i = 3; i > 0; i -= radius)
+    for(double i = 3; i > 1.5; i -= radius)
             tra << QPoint(last.x()+25*sin(i), last.y()+25+25*cos(i));
 
     last = tra.last();
-
-    Feu* feu = new Feu;
-    feu->setCoordonnee(tra.last()+QPoint(0,3));
-    m_obstacleList << feu;
-    feu->setLieu(last);
-    feu->setNom("Feu1");
-    feu->start();
-
-    for(double i = 3; i < 6; i += radius)
-            tra << QPoint(last.x()+25*sin(i), last.y()+25+25*cos(i));
+    for(double i = 1; i < 50; i++)
+            tra << QPoint(last.x(), last.y()+i);
 
     last = tra.last();
-
-    Feu* feu2 = new Feu;
-    feu2->setCoordonnee(tra.last()+QPoint(0,3));
-    m_obstacleList << feu2;
-    feu2->setLieu(last);
-    feu2->setNom("Feu2");
-    feu2->start();
-
-    for(int i = 1; i < 155; i++)
-            tra << QPoint(last.x()+i,last.y());
+    for(double i = -1.5; i < 0; i += radius)
+            tra << QPoint(last.x()+25+25*sin(i), last.y()+25*cos(i));
 
     last = tra.last();
-    for(double i = 3; i > 1.3; i -= radius)
-            tra << QPoint(last.x()+25*sin(i), last.y()+25+25*cos(i));
+    for(double i = 1; i < 75; i++)
+        tra << QPoint(last.x()+i, last.y());
 
 
     last = tra.last();
+    for(double i = 0; i < 1.5; i += radius)
+            tra << QPoint(last.x()+1+25*sin(i), last.y()-25+25*cos(i));
 
-    Station* s2 = new Station(Station::Terminus);
-    s2->setCoordonnee(last+QPoint(5,-3));
-    s2->setLieu(last);
-    s2->setNom("Arrivee");
-    m_obstacleList << s2;
+    last = tra.last();
+    for(double i = 1; i < 50; i++)
+        tra << QPoint(last.x(), last.y()-i);
+
+    last = tra.last();
+    for(double i = 4.5; i > 3; i -= radius)
+            tra << QPoint(last.x()+25+25*sin(i), last.y()+3+25*cos(i));
+
+    last = tra.last();
+    for(double i = 1; i < 50; i++)
+        tra << QPoint(last.x()+i, last.y());
 
     Trajet* traj = new Trajet;
     traj->setTrajet(tra);
     m_trajetList << traj;
 
-    foreach(Obstacle* o, m_obstacleList)
-        traj->addObstacle(o);
+    tra.clear();
+
+    last = QPoint(25,217);
+
+    for(double i = 0; i < 50; i++)
+        tra << QPoint(last.x()+i, last.y());
+
+    last = tra.last();
+    for(double i = 0; i < 1.5; i += radius)
+            tra << QPoint(last.x()+1+25*sin(i), last.y()-25+25*cos(i));
+
+    last = tra.last();
+    for(double i = 1; i < 50; i++)
+            tra << QPoint(last.x(), last.y()-i);
+
+    last = tra.last();
+    for(double i = 4.5; i > 3; i -= radius)
+            tra << QPoint(last.x()+25+25*sin(i), last.y()+3+25*cos(i));
+
+    last = tra.last();
+    for(double i = 1; i < 75; i++)
+        tra << QPoint(last.x()+i, last.y());
+
+    last = tra.last();
+    for(double i = 3; i > 1.5; i -= radius)
+            tra << QPoint(last.x()+25*sin(i), last.y()+25+25*cos(i));
+
+    last = tra.last();
+    for(double i = 1; i < 50; i++)
+            tra << QPoint(last.x(), last.y()+i);
+
+    last = tra.last();
+    for(double i = -1.5; i < 0; i += radius)
+            tra << QPoint(last.x()+25+25*sin(i), last.y()+25*cos(i));
+
+    last = tra.last();
+    for(double i = 1; i < 50; i++)
+        tra << QPoint(last.x()+i, last.y());
+
+
+    Trajet* traj2 = new Trajet;
+    traj2->setTrajet(tra);
+    m_trajetList << traj2;
+
+
 
     foreach(Trajet* t , m_trajetList)
         m_trajetList << t;
