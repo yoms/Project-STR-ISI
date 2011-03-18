dc48b0dd768568eea1cda35d93d394547bae1c28
Guillaume <guillaume@guillaume-1005HA.(none)>
2011-03-18 09:58:04 +0100

    Ajout de la fonction cmake pour ada

--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 #include <QtGui/QApplication>
 #include <QtCore/QTextCodec>
 
+extern "C" {
+extern int Flip_Flop1(void);
+extern void adainit (void);
+extern void adafinal (void);
+}
 int main(int argc, char *argv[])
 {
+    adainit();
+    Flip_Flop1();
     QApplication a(argc, argv);
     QTextCodec::setCodecForTr(QTextCodec::codecForName("System"));
     TramWindow w;
     w.show();
+    adafinal();
     return a.exec();
 }
