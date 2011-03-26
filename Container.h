d3f705108d1d2c334eedddb0f8fd402e1a92e4ac
nico <nico@Nico-PC.(none)>
2011-03-26 19:16:21 +0100

    Ajout des commentaires pour Container

--- Container.h
+++ Container.h
@@ -5,13 +5,35 @@
 
 class Person;
 
+/**
+ * @brief Représente un conteneur de Person.
+ * @see Person
+ */
 class Container
 {
 public:
+    /**
+     * @brief Construit un conteneur de Person.
+     */
     Container();
+    /**
+     * @brief Detruit un conteneur de Person.
+     */
     virtual ~Container();
+    /**
+     * @brief Retourne la liste des Person contenues dans le conteneur.
+     * @return la liste des Person contenues dans le conteneur
+     */
     QList<Person*> persons() const {return this->m_persons;}
+    /**
+     * @brief Fait entrer une personne dans le conteneur.
+     * @param la personne à ajouter au conteneur
+     */
     void enter(Person*);
+    /**
+     * @brief Fait sortir une personne dans le conteneur.
+     * @param la personne à faire sortir du conteneur
+     */
     void quit(Person*);
 private:
     QList<Person*> m_persons;
